3465194ee45983a7b450617b93f29410911d9f0e
inquisitive-entropy <59537201+inquisitive-entropy@users.noreply.github.com>
2021-07-13 07:42:53 -0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+const debug = process.env.NODE_ENV !== 'production';
+
+// This file is not going through babel transformation.
+// So, we write it in vanilla JS
+// (But you could use ES2015 features supported by your Node.js version)
 
 module.exports = withPWA({
   pwa: {
     dest: 'public',
     runtimeCaching,
   },
-})
+  assetPrefix: !debug ? '/gh-pages-next-pwa/' : ''
+});
+
