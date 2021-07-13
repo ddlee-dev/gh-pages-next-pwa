0bf42b1f2b31456cdccfc6ce4f78cac86a12fc34
inquisitive-entropy <59537201+inquisitive-entropy@users.noreply.github.com>
2021-07-13 12:06:35 -0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Next.js Boilerplate 1
+          Next.js Boilerplate 2
         </h1>
 
         <p className={styles.description}>
