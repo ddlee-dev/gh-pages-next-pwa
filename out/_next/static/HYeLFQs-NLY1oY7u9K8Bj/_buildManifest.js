726119b4e6b05454688647cdf86b96429dab363a
inquisitive-entropy <59537201+inquisitive-entropy@users.noreply.github.com>
2021-07-13 12:09:01 -0500

    Deploy Next.js to gh-pages

--- out/_next/static/HYeLFQs-NLY1oY7u9K8Bj/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0bf87fab37fa1b5aa795.css","static/chunks/pages/index-b78ae72f8f4bd3dbf846.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
